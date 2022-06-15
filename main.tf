c171e97b83f5f8677847803d70adbed05b6f3d81
Edgar Mocharnyk <mocharnik@gmail.com>
2022-06-15 14:42:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "resource1" {
   provisioner "local-exec" {
     command = "echo $ENV"
     environment = {
-      ENV = "Hello awesome world, I'm here, again!"
+      ENV = "Hello awesome world, I'm here!"
     }
   }
   triggers = {
